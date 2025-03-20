3e8f1b8e63dbf2c73b79e959b1f2694d1069d26c
Quang Huy <kaheetonaa@gmail.com>
2025-03-20 13:33:47 +0100

    defferedPrompt

--- sw.js
+++ sw.js
@@ -1,53 +1,64 @@
-var GHPATH = 'https://kaheetonaa.github.io/iDforALL';
-var APP_PREFIX = 'iDforALL';
-var VERSION = 'version_0_0_1';
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/land.html`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/dist/iD.css`,
-  `${GHPATH}/dist/iD.js`
+var APP_PREFIX = 'iD4ALL'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/iD4ALL/',                     // If you have separate JS/CSS files,
+  '/iD4ALL/index.html',            // add path to those files here
+  '/iD4ALL/land.html', 
+  '/iD4ALL/redirect.html',
+  '/iD4ALL/tm.html',
+  '/iD4ALL/dist/id.js',
+  '/iD4ALL/dist/id.css',
+  '/iD4ALL/icon/icon-4all.png'    
 ]
 
-var CACHE_NAME = APP_PREFIX + VERSION
+// Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
+  console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { 
-        console.log('Responding with cache : ' + e.request.url);
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
         return request
-      } else {       
-        console.log('File is not cached, fetching : ' + e.request.url);
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   )
 })
 
+// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
+      console.log('installing cache : ' + CACHE_NAME)
       return cache.addAll(URLS)
     })
   )
 })
 
+// Delete outdated caches
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
       var cacheWhitelist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX)
       })
-      cacheWhitelist.push(CACHE_NAME);
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('Deleting cache : ' + keyList[i] );
+          console.log('deleting cache : ' + keyList[i] )
           return caches.delete(keyList[i])
         }
       }))
     })
   )
 })
-
